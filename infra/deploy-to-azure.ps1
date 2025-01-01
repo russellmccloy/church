bf669b0b4674caf191ba381145f5473be604a7de
Russell McCloy <russell.mccloy@googlemail.com>
2025-01-01 12:07:40 +1100

    some infra

---
+++ infra/deploy-to-azure.ps1
@@ -0,0 +1,40 @@
+# Step 1:
+Connect-AzAccount -UseDeviceAuthentication
+
+# Step 2:
+# (Optional) If you have multiple subscriptions, select a default subscription to deploy the resources.
+$context = Get-AzSubscription -SubscriptionName 'RussDefault'
+Set-AzContext $context
+
+$resourceGroupName = 'church-rg'
+$location = 'australiaeast'
+
+# Step 3:
+# Create the resource group.
+$expiry = ((Get-Date).AddDays(1)).ToString('dd/MM/yyyy')
+New-AzResourceGroup -Name $resourceGroupName -Location $location -Force -Tag @{ Owner = 'Russ'; Purpose = 'To contain the resources for Church.'; Expiry = $expiry }
+
+# (Optional) set the default resource group for the subsequent deployments
+Set-AzDefault -ResourceGroupName $resourceGroupName
+
+# Step 4:
+# (Optional) Run Bicep Linter
+az bicep build -f .\infra\main.bicep
+
+# Generate a unique deployment name.
+$deploymentName = 'church-deployment-' + (Get-Date).ToString('yyyyMMddThhmmss')
+
+
+# Step 6:
+# Deploy to Azure
+New-AzResourceGroupDeployment `
+    -Name $deploymentName `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile '.\infra\main.bicep' `
+    -TemplateParameterFile .\infra\environments\main.dev.bicepparam
+
+# Step 7:
+# Clean up resources
+Remove-AzResourceGroup -Name $resourceGroupName -Force
+
+
---
+++ infra/main.bicep
@@ -0,0 +1,28 @@
+@description('The name ofthe CosmosDb account.')
+param cosmosDbName string
+
+@description('Optional. The region where the resources live.')
+@allowed([
+  'australiaeast'
+  'australiasoutheast'
+  'australiacentral'
+  'australiasoutheast'
+])
+param location string = 'australiaeast'
+
+resource cosmosdb 'Microsoft.DocumentDB/databaseAccounts@2021-04-15' = {
+  name: cosmosDbName
+  location: location
+  kind: 'GlobalDocumentDB'
+  properties: {
+    databaseAccountOfferType: 'Standard'
+    locations: [
+      {
+        locationName: 'Australia East'
+        failoverPriority: 0
+      }
+    ]
+    enableFreeTier: true
+    li
+  }
+}
